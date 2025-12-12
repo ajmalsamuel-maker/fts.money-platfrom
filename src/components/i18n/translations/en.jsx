93fbb52bf6572a94ab794896c02b6be68d7abb3f
Ajmal Victor Samuel <ajmalsamuel@gmail.com>
2025-12-12 15:08:38 +0000

    File changes

--- src/components/i18n/translations/en.jsx
+++ src/components/i18n/translations/en.jsx
@@ -4,5 +4,12 @@ json
     "merchantPricing": "Merchant Pricing",
     "midPricingConfig": "MID Pricing Config",
     "invoices": "Invoices",
-    "feeTypeManagement": "Fee Type Management"
+    "feeTypeManagement": "Fee Type Management",
+    "subMerchants": "Sub-Merchants",
+    "instantPayments": "Instant Payments",
+    "networkTokenization": "Network Tokenization",
+    "accountUpdater": "Account Updater",
+    "smartRetry": "Smart Retry",
+    "pciCompliance": "PCI Compliance",
+    "checkoutComponents": "Checkout Components"
 }
