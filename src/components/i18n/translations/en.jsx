bfe8d326bb6d277285b0310782a0ce85bac7488c
Ajmal Victor Samuel <ajmalsamuel@gmail.com>
2025-12-12 11:42:10 +0000

    File changes

---
+++ src/components/i18n/translations/en.jsx
@@ -0,0 +1,8 @@
+json
+{
+    "buyRates": "Buy Rates",
+    "merchantPricing": "Merchant Pricing",
+    "midPricingConfig": "MID Pricing Config",
+    "invoices": "Invoices",
+    "feeTypeManagement": "Fee Type Management"
+}
