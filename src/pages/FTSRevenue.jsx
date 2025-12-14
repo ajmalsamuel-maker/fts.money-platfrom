0cb217590e1541c0902cfa49e78628f6c813773c
Ajmal Victor Samuel <ajmalsamuel@gmail.com>
2025-12-14 10:10:36 +0000

    File changes

--- src/pages/FTSRevenue.jsx
+++ src/pages/FTSRevenue.jsx
@@ -121,6 +121,7 @@ export default function FTSRevenue() {
                         </Table>
                     </CardContent>
                 </Card>
+                </div>
             </div>
         </div>
     );
