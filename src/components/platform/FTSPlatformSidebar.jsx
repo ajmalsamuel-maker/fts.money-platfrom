d2abe1b1c329e2e9a9e3040ba485cb7c30b9878e
Ajmal Victor Samuel <ajmalsamuel@gmail.com>
2025-12-20 13:33:02 +0000

    File changes

--- src/components/platform/FTSPlatformSidebar.jsx
+++ src/components/platform/FTSPlatformSidebar.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import { cn } from "@/lib/utils";
 import { Badge } from "@/components/ui/badge";
-import React from 'react';
 import { 
     Building2, 
     BarChart3, 
