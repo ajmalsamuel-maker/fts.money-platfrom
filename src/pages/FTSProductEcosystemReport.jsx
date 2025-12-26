2cba4dc064ad0e246637ebf07c7e5764667d7ef0
Ajmal Victor Samuel <ajmalsamuel@gmail.com>
2025-12-26 13:24:20 +0000

    File changes

--- src/pages/FTSProductEcosystemReport.jsx
+++ src/pages/FTSProductEcosystemReport.jsx
@@ -196,7 +196,7 @@ export default function FTSProductEcosystemReport() {
                                         },
                                     }}
                                 >
-                                    {ArchitectureDoc}
+                                    {ProductEcosystemDoc}
                                 </ReactMarkdown>
                             </div>
                         </CardContent>
