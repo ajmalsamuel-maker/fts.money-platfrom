159d03d46792cff005755f4c78c75dd815983d55
Ajmal Victor Samuel <ajmalsamuel@gmail.com>
2025-12-28 08:26:14 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -255,6 +255,7 @@ import Webhooks from './pages/Webhooks';
 import WorkflowManagement from './pages/WorkflowManagement';
 import WorkflowTemplateLibrary from './pages/WorkflowTemplateLibrary';
 import XeroIntegration from './pages/XeroIntegration';
+import AssetIssuerCompliance from './pages/AssetIssuerCompliance';
 import __Layout from './Layout.jsx';
 
 
@@ -516,6 +517,7 @@ export const PAGES = {
     "WorkflowManagement": WorkflowManagement,
     "WorkflowTemplateLibrary": WorkflowTemplateLibrary,
     "XeroIntegration": XeroIntegration,
+    "AssetIssuerCompliance": AssetIssuerCompliance,
 }
 
 export const pagesConfig = {
