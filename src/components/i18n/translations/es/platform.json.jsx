70d100c1bdcbbe3471e7920e8173539f7bba2a70
Ajmal Victor Samuel <ajmalsamuel@gmail.com>
2026-01-03 03:59:43 +0000

    File changes

---
+++ src/components/i18n/translations/es/platform.json.jsx
@@ -0,0 +1,9 @@
+{
+  "dashboard": {
+    "title": "Panel de Control",
+    "subtitle": "Gestión unificada para todas las instancias PSP y configuraciones globales"
+  },
+  "services": {
+    "title": "Servicios de Plataforma"
+  }
+}
