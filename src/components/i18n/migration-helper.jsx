ecbecafa6266e0736bb7e6fa1c6ef17756b0e168
Ajmal Victor Samuel <ajmalsamuel@gmail.com>
2026-01-04 02:51:41 +0000

    File changes

---
+++ src/components/i18n/migration-helper.jsx
@@ -0,0 +1,6 @@
+/**
+ * Temporary re-export for backwards compatibility
+ * This allows old imports to still work during migration
+ */
+
+export { useI18n, SUPPORTED_LANGUAGES, I18nextProvider as EnhancedLanguageProvider } from './I18nextProvider';
