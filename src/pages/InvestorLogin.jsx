e5f05c13c3fb673b8f4c59d3a0e7a775be796302
Ajmal Victor Samuel <ajmalsamuel@gmail.com>
2026-01-07 13:14:09 +0000

    File changes

--- src/pages/InvestorLogin.jsx
+++ src/pages/InvestorLogin.jsx
@@ -43,7 +43,7 @@ export default function InvestorLogin() {
     return (
         <div className="min-h-screen bg-white flex flex-col">
             <div className="py-4 px-6 flex justify-end">
-                <LanguageSwitcher variant="compact" />
+                <LanguageSwitcher variant="select" />
             </div>
 
             <div className="flex-1 flex items-center justify-center p-6 relative overflow-hidden">
@@ -105,14 +105,10 @@ export default function InvestorLogin() {
                 </Card>
             </div>
 
-            {/* Wave SVG under the login area */}
-            <div className="relative -mt-32 pointer-events-none">
-                <svg viewBox="0 0 1440 320" className="w-full h-auto">
-                    <path 
-                        fill="#dbeafe" 
-                        fillOpacity="1" 
-                        d="M0,96L48,112C96,128,192,160,288,160C384,160,480,128,576,122.7C672,117,768,139,864,138.7C960,139,1056,117,1152,106.7C1248,96,1344,96,1392,96L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
-                    ></path>
+            {/* Wave Background */}
+            <div className="absolute bottom-0 left-0 right-0 pointer-events-none">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className="w-full">
+                    <path fill="#1e3a8a" fillOpacity="0.1" d="M0,96L48,112C96,128,192,160,288,176C384,192,480,192,576,170.7C672,149,768,107,864,101.3C960,96,1056,128,1152,138.7C1248,149,1344,139,1392,133.3L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
                 </svg>
             </div>
 
