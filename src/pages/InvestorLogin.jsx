414fafdda86eb17b3a8f5c95cfa0b71685d06007
Ajmal Victor Samuel <ajmalsamuel@gmail.com>
2026-01-07 13:16:40 +0000

    File changes

--- src/pages/InvestorLogin.jsx
+++ src/pages/InvestorLogin.jsx
@@ -105,10 +105,17 @@ export default function InvestorLogin() {
                 </Card>
             </div>
 
-            {/* Wave Background */}
-            <div className="absolute bottom-0 left-0 right-0 pointer-events-none">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className="w-full">
-                    <path fill="#1e3a8a" fillOpacity="0.1" d="M0,96L48,112C96,128,192,160,288,176C384,192,480,192,576,170.7C672,149,768,107,864,101.3C960,96,1056,128,1152,138.7C1248,149,1344,139,1392,133.3L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+            {/* FTS.Money Wave Background - Bottom */}
+            <div className="absolute bottom-0 left-0 right-0 h-1/3">
+                <svg className="absolute bottom-0 w-full h-full" viewBox="0 0 1440 320" preserveAspectRatio="none">
+                    <path fill="url(#wave-gradient)" fillOpacity="1" d="M0,192L48,197.3C96,203,192,213,288,229.3C384,245,480,267,576,250.7C672,235,768,181,864,181.3C960,181,1056,235,1152,234.7C1248,235,1344,181,1392,154.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+                    <defs>
+                        <linearGradient id="wave-gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+                            <stop offset="0%" style={{ stopColor: '#0066CC', stopOpacity: 0.8 }} />
+                            <stop offset="50%" style={{ stopColor: '#00BFFF', stopOpacity: 0.8 }} />
+                            <stop offset="100%" style={{ stopColor: '#87CEEB', stopOpacity: 0.7 }} />
+                        </linearGradient>
+                    </defs>
                 </svg>
             </div>
 
