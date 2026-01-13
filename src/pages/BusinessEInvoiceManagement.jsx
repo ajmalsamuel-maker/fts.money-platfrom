dc3faddb418f69a091d827236a8fb26748339983
Ajmal Victor Samuel <ajmalsamuel@gmail.com>
2026-01-13 13:35:57 +0000

    File changes

--- src/pages/BusinessEInvoiceManagement.jsx
+++ src/pages/BusinessEInvoiceManagement.jsx
@@ -24,6 +24,7 @@ export default function BusinessEInvoiceManagement() {
         business_email: '',
         country: '',
         tax_id: '',
+        tax_submission_standards: '',
         status: 'active'
     });
 
