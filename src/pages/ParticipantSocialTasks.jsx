1abbefde5a1e148b5cca1b62eaf9a603be734a61
Ajmal Victor Samuel <ajmalsamuel@gmail.com>
2026-01-13 09:24:53 +0000

    File changes

--- src/pages/ParticipantSocialTasks.jsx
+++ src/pages/ParticipantSocialTasks.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { Link } from 'react-router-dom';
 import { base44 } from '@/api/base44Client';
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
