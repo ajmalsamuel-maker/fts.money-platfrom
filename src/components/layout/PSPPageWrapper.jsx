b6df7aade3aa80d7b583f1826058b4383ee12e97
Ajmal Victor Samuel <ajmalsamuel@gmail.com>
2026-01-17 11:52:22 +0000

    File changes

--- src/components/layout/PSPPageWrapper.jsx
+++ src/components/layout/PSPPageWrapper.jsx
@@ -25,7 +25,7 @@ export default function PSPPageWrapper({ children, currentPage }) {
     }, []);
 
     return (
-        <div className="min-h-screen bg-slate-50">
+        <div className="min-h-screen bg-slate-50 flex">
             {!sidebarCollapsed && (
                 <div 
                     className="fixed inset-0 bg-black/50 z-30 lg:hidden"
@@ -36,15 +36,16 @@ export default function PSPPageWrapper({ children, currentPage }) {
             <Sidebar 
                 collapsed={sidebarCollapsed} 
                 currentPage={currentPage}
+                onToggle={() => setSidebarCollapsed(!sidebarCollapsed)}
             />
             
-            <div className={cn("transition-all duration-300", sidebarCollapsed ? "ml-20" : "lg:ml-64 ml-40")}>
+            <div className="flex-1 flex flex-col min-h-screen">
                 <TopHeader 
                     onToggleSidebar={() => setSidebarCollapsed(!sidebarCollapsed)}
                     collapsed={sidebarCollapsed}
                 />
                 
-                <main className="p-6">
+                <main className="flex-1 p-4 sm:p-6 overflow-auto">
                     {children}
                 </main>
             </div>
